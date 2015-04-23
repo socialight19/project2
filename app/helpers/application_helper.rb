2ad98d67495740743e8a8f940440bab847476bf6
socialight21 <vcarloscruz@gmail.com>
2015-04-23 16:39:56 -0700

    fix delete auth on your own profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def edit_access(question)
-		if logged_in? && current_user.id == question.asked_by
+		if (logged_in? && current_user.id == question.asked_by) || (logged_in? && current_user.id == question.user_id)
 			return true
 		else 
 			false
